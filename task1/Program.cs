07fc0711d3cf4a75fe259b87be1b4aee71345d3b
Artem <styoganov@bk.ru>
2022-08-14 17:44:38 +0300

    task2

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+Console.WriteLine("Введи число: ");
+int number = int.Parse(Console.ReadLine());
+// int anyNumber = Convert.ToInt32(Console.ReadLine());
+// string anyNumberText = Convert.ToString(anyNumber);
+if (number > 100)
+{
+    int thirddigit = number % 10;
+    Console.WriteLine($"третья цифра -> {thirddigit}");
+}
+else
+{
+    Console.WriteLine("-> третьей цифры нет");
+}
+
