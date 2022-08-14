33f2d3759a0717b4d9c41e584c1fe7d90c7db826
Artem <styoganov@bk.ru>
2022-08-14 17:17:03 +0300

    task1

---
+++ task0/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.WriteLine("Ввдедите трехзнасное число: ");
+int number = int.Parse(Console.ReadLine());
+void MaxDigit(int num)
+{
+    int firstDigit = num / 100;
+    int firstpos= firstDigit * 100; 
+    int thidrpos = num % 10; 
+    int numberLast = (num - firstpos - thidrpos) / 10;
+    Console.WriteLine($"{number} -> {numberLast}");
+    
+} 
+
+MaxDigit(number);
